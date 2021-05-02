41acecc0be2fd7f3905bdf7af84dc97b05f42203
Vladimir Khvan <vladimirkhvan23@gmail.com>
2021-05-02 22:54:13 +0500

    done

--- app/Post.php
+++ app/Post.php
@@ -2,53 +2,19 @@
 
 namespace App;
 
-class Post
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public function getPosts($session)
+    protected $fillable = ['title', 'content'];
+
+    public function likes()
     {
-        if (!$session->has('posts')) {
-            $this->createDummyData($session);
-        }
-        return $session->get('posts');
+        return $this->hasMany('App\Like', 'post_id');
     }
 
-    public function getPost($session, $id)
+    public function tags()
     {
-        if (!$session->has('posts')) {
-            $this->createDummyData();
-        }
-        return $session->get('posts')[$id];
-    }
-
-    public function addPost($session, $title, $content)
-    {
-        if (!$session->has('posts')) {
-            $this->createDummyData();
-        }
-        $posts = $session->get('posts');
-        array_push($posts, ['title' => $title, 'content' => $content]);
-        $session->put('posts', $posts);
-    }
-
-    public function editPost($session, $id, $title, $content)
-    {
-        $posts = $session->get('posts');
-        $posts[$id] = ['title' => $title, 'content' => $content];
-        $session->put('posts', $posts);
-    }
-
-    private function createDummyData($session)
-    {
-        $posts = [
-            [
-                'title' => 'Learning Laravel',
-                'content' => 'This blog post will get you right on track with Laravel!'
-            ],
-            [
-                'title' => 'Something else',
-                'content' => 'Some other content'
-            ]
-        ];
-        $session->put('posts', $posts);
+        return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
     }
 }
